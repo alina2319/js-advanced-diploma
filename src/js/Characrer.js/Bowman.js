dd7a1b0ec1b2c8e526bb6b4cb4a2c6711fb3a28f
Alina <Alin.k4@yandex.ru>
2023-06-21 16:06:07 +0300

    first commit ads

--- src/js/Characrer.js/Bowman.js
+++ src/js/Characrer.js/Bowman.js
@@ -1,11 +1,10 @@
 import  Character  from '../Character';
 export default class Bowman extends Character {
-    constructor(level) {
+    constructor(level, 'bowman') {
         super(level);
         this.attack = 25;
         this.defence = 25;
         this.health = 100;
-        this.type = 'bowman';
         this.distance = 2 ;
         this.distanceAttack = 2;
     }
